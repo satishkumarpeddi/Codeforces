1589323fb89b8ebe149fadf9a8295927b20f99cf
evhook <anonymous1986222@gmail.com>
2026-02-22 07:39:54 +0530

    Refactor WayTooLongWords to handle multiple test cases

--- Problem800/WayTooLongWords.java
+++ Problem800/WayTooLongWords.java
@@ -3,12 +3,19 @@ import java.io.*;
 public class WayTooLongWords {
     public static void main(String[] args)throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        String str = bf.readLine().trim();
-        String result = "";
-        result +=str.charAt(0);
-        result +=String.valueOf(str.length()-2);
-        result +=str.charAt(str.length()-1);
-        System.out.println(result);
+        int testCases = Integer.parseInt(bf.readLine().trim());
+        while(testCases-->0){
+            String str = bf.readLine().trim();
+            if(str.length()>10){
+                String result = "";
+                result +=str.charAt(0);
+                result +=String.valueOf(str.length()-2);
+                result +=str.charAt(str.length()-1);
+                System.out.println(result);
+            }else{
+                System.out.println(str);
+            }
+        }
         
     }
 }
