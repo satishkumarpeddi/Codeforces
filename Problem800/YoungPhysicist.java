900e4f86701f02f8114698f17b7204a0c09205be
evhook <anonymous1986222@gmail.com>
2026-02-24 19:12:48 +0530

    Refactor Young Physicist solution to calculate individual vector sums

--- Problem800/YoungPhysicist.java
+++ Problem800/YoungPhysicist.java
@@ -1,15 +1,18 @@
 package Problem800;
 import java.io.*;
+import java.util.*;
 public class YoungPhysicist {
     public static void main(String[] args)throws IOException{
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(bf.readLine().trim());
-        int sum = 0;
+        int sumX = 0,sumY=0,sumZ=0;
         for(int i=0;i<n;i++){
             String[] line = bf.readLine().trim().split("\\s+");
-            for(int j=0;j<line.length;j++)
-                sum+=Integer.parseInt(line[j]); 
+            // System.out.println(Arrays.toString(line));
+            sumX+=Integer.parseInt(line[0]);
+            sumY+=Integer.parseInt(line[1]);
+            sumZ+=Integer.parseInt(line[2]);
         }
-        System.out.println(sum==0?"YES":"NO");
+        System.out.println(sumX==0&&sumY==0&&sumZ==0?"YES":"NO");
     }
 }
